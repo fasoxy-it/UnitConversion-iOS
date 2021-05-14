7f326d808b4ac3da6b63a2f59470c56947336332
Mattia Fasoli <mattia.fasoli@socialthingum.com>
2021-05-14 12:52:14 +0200

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Mattia Fasoli on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
